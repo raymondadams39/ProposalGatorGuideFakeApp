55a37d44cd00ec3ddb39254ef694320d4131ddc3
raymondadams <raymondaadams39@gmail.com>
2026-04-20 22:48:02 -0400

    updated server to handle images

--- server.js
+++ server.js
@@ -2,7 +2,32 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
+const mimeTypes = {
+    '.html': 'text/html',
+    '.png': 'image/png',
+    '.jpg': 'image/jpeg',
+    '.jpeg': 'image/jpeg',
+    '.gif': 'image/gif',
+    '.svg': 'image/svg+xml',
+    '.js': 'application/javascript',
+    '.css': 'text/css',
+};
+
 http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    fs.createReadStream(path.join(__dirname, 'gatorguide.html')).pipe(res);
+    const filePath = req.url === '/'
+        ? path.join(__dirname, 'gatorguide.html')
+        : path.join(__dirname, req.url);
+
+    const ext = path.extname(filePath);
+    const contentType = mimeTypes[ext] || 'text/plain';
+
+    fs.readFile(filePath, (err, data) => {
+        if (err) {
+            res.writeHead(404);
+            res.end('Not found');
+            return;
+        }
+        res.writeHead(200, { 'Content-Type': contentType });
+        res.end(data);
+    });
 }).listen(process.env.PORT || 8080);
